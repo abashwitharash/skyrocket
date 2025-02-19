95699c496181f6f32b6afc24b061af09e4b12bf4
Arash <arashsaleh798@gmail.com>
2025-02-19 09:23:22 -0800

    application schema created

--- models/user.js
+++ models/user.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+// creating a modificaiton to the schema...adding a sub..like a child 
+const applicationsSchema = new mongoose.Schema({
+  company: {
+    type: String, 
+    required: true,
+  },
+  title: {
+    type: String, 
+    required: true,
+  },
+  notes: {
+    type: String, 
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String, 
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +31,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationsSchema],
 });
 
 const User = mongoose.model('User', userSchema);
